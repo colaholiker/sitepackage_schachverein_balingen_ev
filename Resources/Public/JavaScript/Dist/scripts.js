7945b8e645e90b6e8ae2d04dbfa417fed4926285
colaholiker <github@waskurzes.de>
2023-06-07 10:43:07 +0200

    aufräumen

--- Resources/Public/JavaScript/Dist/scripts.js
+++
@@ -1,6 +0,0 @@
-/*!
- * Sitepackage Schachverein Balingen e.V. v1.0.0 (https://www.svbalingen.de/)
- * Copyright 2017-2023 Sebastian During
- * Licensed under the GPL-2.0-or-later license
- */
-console.log("WE LOVE TYPO3");
